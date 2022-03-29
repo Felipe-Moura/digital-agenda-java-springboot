2be76089d7600472c26d81ca35ffbd549259581b
Felipe Moura <flemc00@gmail.com>
2022-03-28 22:35:44 -0300

    First commit, user crud done

---
+++ src/main/java/com/web/agenda/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.web.agenda.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.web.agenda.models.UserModel;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, UUID>{
+
+}
